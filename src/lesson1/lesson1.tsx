aac0cc37f00397b4ecbdc272f8874dd7147d52ed
zhankaam <zhankaam@gmail.com>
2021-06-03 20:00:11 +0300

    Initial commit

---
+++ src/lesson1/lesson1.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CurrencyExchangeContainer from '../containers/CurrencyExchangeContainer/CurrencyEContainer'
+
+
+const Lesson1: React.FunctionComponent = () => {
+    return (
+    <React.Fragment>
+        <CurrencyExchangeContainer />
+    </React.Fragment>
+  );
+};
+
+export default Lesson1;
+
---
+++ src/redux/actions.ts
@@ -0,0 +1,61 @@
+import {useDispatch as _useDispatch} from "react-redux";
+
+export enum ACTIONS_TYPE {
+    CHANGE_CURRENCY_FIELD_TYPE = 'CurrencyExchange/CHANGE_CURRENCY_FIELD_TYPE',
+    CHANGE_CHANGE_ACTION = 'CurrencyExchange/CHANGE_CHANGE_ACTION',
+    CHANGE_CURRENT_CURRENCY = 'CurrencyExchange/CHANGE_CURRENT_CURRENCY',
+}
+
+
+export type ChangeCurrencyFieldType = {
+    type: ACTIONS_TYPE.CHANGE_CURRENCY_FIELD_TYPE,
+    payload : {
+        amountOfBYN: string,
+        amountOfCurrency: string
+    }
+};
+
+
+export const ChangeCurrencyFieldAC = (amountOfBYN: string, amountOfCurrency: string): ChangeCurrencyFieldType => {
+   return {
+       type: ACTIONS_TYPE.CHANGE_CURRENCY_FIELD_TYPE,
+       payload: {
+           amountOfBYN,
+           amountOfCurrency
+       }
+   }
+};
+
+export type ChangeAction = {
+    type: ACTIONS_TYPE.CHANGE_CHANGE_ACTION,
+    payload: {isBuying: boolean}
+};
+
+
+export const ChangeActionAC = (isBuying: boolean): ChangeAction => {
+    return {
+        type: ACTIONS_TYPE.CHANGE_CHANGE_ACTION,
+        payload: {isBuying}
+    }
+};
+
+export type ChangeCurrentCurrencyType = {
+    type: ACTIONS_TYPE.CHANGE_CURRENT_CURRENCY,
+    payload: {currentCurrency: string}
+};
+
+
+export const ChangeCurrentCurrencyAC = (currentCurrency: string): ChangeCurrentCurrencyType => {
+    return {
+        type: ACTIONS_TYPE.CHANGE_CURRENT_CURRENCY,
+        payload: {currentCurrency}
+    }
+};
+
+export type CurrencyReducersTypes = ChangeCurrencyFieldType | ChangeAction | ChangeCurrentCurrencyType;
+
+export function useDispatch() {
+    const dispatch = _useDispatch()
+    return (ac: CurrencyReducersTypes) => dispatch(ac)
+
+}
